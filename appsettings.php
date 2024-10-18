8ffff7ab9fec056b4817f66c6169987cb15e6be8
Vilhena's Code Lab <93561947+mycents@users.noreply.github.com>
2024-10-18 00:21:42 -0300

    Create appsettings.php

---
+++ appsettings.php
@@ -0,0 +1,8 @@
+<?php return
+[
+    "service_name" => "ApplicationName",
+    "environment" => "Development",
+    "server_url" => "http://apm-server:8200",
+    "secret_token" => "apm-server-secret-token",
+    "version" => "1.0",
+];
